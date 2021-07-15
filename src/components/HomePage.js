f1a7cbf8ee3643dc11d7ccec3cb4b2cb92c3a8df
es99dev <42253390+es99dev@users.noreply.github.com>
2021-07-15 13:39:40 -0500

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -34,7 +34,7 @@ class HomePage extends React.Component {
     this.props.dispatch(setPanel(0))
   }
   componentDidMount() {
-    fetch('https://lws.impactpreview.com//wp-json/wp/v2/pages/120')
+    fetch('/wp-json/wp/v2/pages/120')
       .then(res => res.json())
       .then(result => {
         this.setState({
